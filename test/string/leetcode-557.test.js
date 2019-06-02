f428d47c9824649ce150e3a3aa9c7c9555e4d15e
yyccQQu <643929860@qq.com>
2019-06-02 21:30:29 +0700

    leetcode no.1

---
+++ test/string/leetcode-557.test.js
@@ -0,0 +1,7 @@
+import revertByWorld from '../../code/string/leetcode-557'
+
+test("revertByWorld:Let's take LeetCode contest", () => {
+	expect(revertByWorld("Let's take LeetCode contest")).toBe(
+		"s'teL ekat edoCteeL tsetnoc"
+	);
+});
